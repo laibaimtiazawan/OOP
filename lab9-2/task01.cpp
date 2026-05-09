29e9b1febc5be595ca08b5897c6c180908cc2224
laibaimtiazawan <laiba.imtiazawan@gmail.com>
2026-05-09 20:52:46 +0500

    Add files via upload

---
+++ lab9-2/task01.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+public:
+    virtual double calculateSalary()
+    {
+        cout << "Base Employee salary calculation called." << endl;
+        return 0;
+    }
+};
+
+class PermanentEmployee : public Employee
+{
+private:
+    double basicSalary, bonus;
+
+public:
+    PermanentEmployee(double bs, double b)
+    {
+        basicSalary = bs;
+        bonus = b;
+    }
+
+    double calculateSalary() override
+    {
+        return basicSalary + bonus;
+    }
+};
+
+class ContractEmployee : public Employee
+{
+private:
+    double hourlyRate, hoursWorked;
+
+public:
+    ContractEmployee(double hr, double hw)
+    {
+        hourlyRate = hr;
+        hoursWorked = hw;
+    }
+
+    double calculateSalary() override
+    {
+        return hourlyRate * hoursWorked;
+    }
+};
+
+int main()
+{
+    Employee *emp;
+
+    PermanentEmployee p1(40000, 5000);
+    ContractEmployee c1(800, 30);
+
+    emp = &p1;
+    cout << "Permanent Employee Salary: " << emp->calculateSalary() << endl;
+
+    emp = &c1;
+    cout << "Contract Employee Salary: " << emp->calculateSalary() << endl;
+
+    return 0;
+}
